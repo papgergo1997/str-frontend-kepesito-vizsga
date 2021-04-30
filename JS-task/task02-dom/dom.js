5e6c8a0af797fd78ed31a2180587a3468acb9277
papgergo1997 <pap.gergo.1997@gmail.com>
2021-04-30 10:52:24 +0200

    dom modification nav el display flex added

--- JS-task/task02-dom/dom.js
+++ JS-task/task02-dom/dom.js
@@ -1,6 +1,11 @@
 function changeOuterLinks() {
-    const navElements = document.querySelectorAll("nav#link-list a")
+    const navElement = document.querySelector("nav#link-list");
+    const aElements = document.querySelectorAll("nav#link-list a")
         .forEach(el => el.innerHTML.includes('outer') ? el.setAttribute('target', '_blank') : !el);
+
+    navElement.style.display = 'flex';
+    // navElement.style.flexDirection = 'column';
+    // navElement.style.width = '30%';
 }
 
 changeOuterLinks()
