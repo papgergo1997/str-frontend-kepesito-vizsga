3cf2ad72c4311ab173d912584f63c69d59b2289c
papgergo1997 <pap.gergo.1997@gmail.com>
2021-04-30 12:08:35 +0200

    getMovies method crated

--- angular-cinema-project/cinema/src/app/movie-list/movie-list.component.ts
+++ angular-cinema-project/cinema/src/app/movie-list/movie-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Movie } from '../model/movie';
+import { HttpService } from '../service/http.service';
 
 @Component({
   selector: 'app-movie-list',
@@ -8,15 +10,17 @@ import { Movie } from '../model/movie';
 })
 export class MovieListComponent implements OnInit {
 
-  movies: Movie[] = [];
+  movies: BehaviorSubject<Movie[]> = new BehaviorSubject<Movie[]>([]);
 
-  constructor() { }
+  constructor(private httpService: HttpService) { }
 
   ngOnInit(): void {
   }
 
-  getMovies(): void {}
+  getMovies(): void {
+    this.movies = this.httpService.movieList$;
+  }
 
-  deleteMovie(id: number): any {}
+  deleteMovie(id: number): any { }
 
 }
--- angular-cinema-project/cinema/src/app/service/http.service.ts
+++ angular-cinema-project/cinema/src/app/service/http.service.ts
@@ -1,19 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Movie } from '../model/movie';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  BASE_URL = 'http://localhost:3000/movies';
+  apiURL: string = 'https://tr360-frontend-exam-april.azurewebsites.net/papgergo1997/movies';
+  movieList$: BehaviorSubject<Movie[]> = new BehaviorSubject<Movie[]>([]);
 
-  constructor(undefined) { }
+  constructor(private http: HttpClient) { }
 
-  getMovieList():any {
-    return null;
+  getMovieList(): void {
+    this.http.get<Movie[]>(this.apiURL).subscribe(list => this.movieList$.next(list))
   }
 
-  deleteMovie(id):any {
+  deleteMovie(id): any {
     return null;
   }
 }
