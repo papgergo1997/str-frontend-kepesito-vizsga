07f01eb1d2043ff62f42c48968a1d4dc596baadf
papgergo1997 <pap.gergo.1997@gmail.com>
2021-04-30 10:44:27 +0200

    dom modification target adding cleared

--- JS-task/task02-dom/dom.js
+++ JS-task/task02-dom/dom.js
@@ -1,10 +1,6 @@
 function changeOuterLinks() {
-    const navElements = document.querySelectorAll("nav#link-list a");
-    for (let i = 0; i < navElements.length; i++) {
-        if (navElements[i].innerHTML.includes('outer')) {
-            navElements[i].setAttribute('target', '_blank');
-        }
-    }
+    const navElements = document.querySelectorAll("nav#link-list a")
+        .forEach(el => el.innerHTML.includes('outer') ? el.setAttribute('target', '_blank') : !el);
 }
 
 changeOuterLinks()
