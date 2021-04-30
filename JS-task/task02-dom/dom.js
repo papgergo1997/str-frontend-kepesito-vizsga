0a68c199faba93c98163a4483fd9cd6d6197dc21
papgergo1997 <pap.gergo.1997@gmail.com>
2021-04-30 10:40:57 +0200

    dom modification target adding works fine

--- JS-task/task02-dom/dom.js
+++ JS-task/task02-dom/dom.js
@@ -1,4 +1,11 @@
-function changeOuterLinks() {}
+function changeOuterLinks() {
+    const navElements = document.querySelectorAll("nav#link-list a");
+    for (let i = 0; i < navElements.length; i++) {
+        if (navElements[i].innerHTML.includes('outer')) {
+            navElements[i].setAttribute('target', '_blank');
+        }
+    }
+}
 
-
+changeOuterLinks()
 export { changeOuterLinks };
